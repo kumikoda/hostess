fadbc998787630af893c1374245899e60f37d2a1
kumikoda <anson.ch@gmail.com>
2012-07-25 09:34:07 -0700

    added backbone

---
+++ host.js
@@ -0,0 +1,77 @@
+var Party = Backbone.Model.extend({});
+var PartyList = Backbone.Collection.extend({});
+
+var PartyView = Backbone.View.extend({
+	tagName:'tr',
+	id:'party-view',
+	className:'party',
+	render: function() {
+		var name =this.model.get('name');
+		var size = this.model.get('size');
+		var time = this.model.get('time');
+		var html =  "<td>" + name + "</td>"
+					+ "<td>" + size + "</td>"
+					+ "<td>" + time + "</td>";
+					
+		this.$el.html(html);
+		return this;
+	}
+});
+
+var PartyListView = Backbone.View.extend({
+	tagName: 'tbody',
+	el:$('#party-list'),
+	
+	initialize: function() {
+		this.collection.on('add', this.addOne, this);
+		this.collection.on('reset', this.addAll, this);
+	},
+	
+	render: function() {
+		this.addAll();
+		return this;
+	},
+	
+	addAll: function() {
+		this.collection.forEach(this.addOne, this);
+	},
+	
+	addOne: function(party){
+		var partyView = new PartyView({model:party});
+		this.$el.append(partyView.render().el);
+	}
+
+});
+
+var App = new ( Backbone.Router.extend({
+	el: $("#host-app"),
+	
+	events: {
+      "click #new-party":  "create"
+    },
+	
+	initialize: function(){
+		this.partyList = new PartyList();
+		this.partyListView = new PartyListView({collection:this.partyList});
+	},
+	
+	create: function(){
+		console.log('hello');
+	},
+	
+	test: function() {
+		this.partyList.reset([{name:"anson",size:4,time:"10-15"}, {name:"matthew",size:3,time:"3-5"}]);
+	},
+	
+	addTest: function() {
+		this.partyList.add(new Party({name:"peter", size:1,time:'10-15'		}))
+	}
+
+}));
+
+
+$(function() {
+	
+
+
+});
